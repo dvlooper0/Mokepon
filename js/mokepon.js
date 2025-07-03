6041ad7dda0ba029cabd4ede7168d3c61b5ae89b
dvlooper <dev.soft527@gmail.com>
2025-07-03 09:36:42 -0600

    Movilidad de Mokepon por medio de teclado

--- js/mokepon.js
+++ js/mokepon.js
@@ -137,8 +137,8 @@ function selecionarMascotaJugador(){
     sectionSeleccionarMascota.style.display='none'
     //sectionSeleccionarAtaque.style.display='flex'
     sectionVerMapa.style.display='flex'
-    intervalo = setInterval(pintarPersonaje,50)
-        
+    
+    iniciarMapa()
 
     if(inputHipodoge.checked){
         spanMascotaJugador.innerHTML=inputHipodoge.id
@@ -395,6 +395,39 @@ function moverArriba(){
 function detenerMovimiento(){
     capipepo.velocidadX = 0
     capipepo.velocidadY = 0
+}
+
+function sePresionoUnaTecla(event){
+
+    switch(event.key){
+
+        case 'ArrowUp':
+            moverArriba()
+        break
+
+        case 'ArrowDown':
+            moverAbajo()
+        break
+
+        case 'ArrowLeft':
+            moverIzquierda()
+        break
+
+        case 'ArrowRight':
+            moverDerecha()
+        break
+
+        default:
+            break
+    }
+
+}
+
+function iniciarMapa(){
+    intervalo = setInterval(pintarPersonaje,50)
+
+    window.addEventListener('keydown', sePresionoUnaTecla)
+    window.addEventListener('keyup', detenerMovimiento)
 }
 
 window.addEventListener('load',iniciarJuego) 
