9758fb14da14650cdccb8bfbe830975834b57ec9
dvlooper <dev.soft527@gmail.com>
2025-07-13 20:12:15 -0600

    Correcciones -menores-

--- js/mokepon.js
+++ js/mokepon.js
@@ -21,6 +21,7 @@ const mapa=document.getElementById('mapa')
 
 let jugadorId = null
 let mokepones=[]
+let mokeponesEnemigos = []
 let ataqueJugador=[]
 let ataqueEnemigo=[]
 let opcionDeMokepones
@@ -456,13 +457,18 @@ function pintarCanvas(){
     mascotaJugadorObjeto.pintarMokepon()
 
     enviarPosicion(mascotaJugadorObjeto.x,mascotaJugadorObjeto.y)
+
+    mokeponesEnemigos.forEach(function (mokepon){
+
+        mokepon.pintarMokepon()
+    })
 /*
     hipodogeEnemigo.pintarMokepon()
     capipepoEnemigo.pintarMokepon()
     ratigueyaEnemigo.pintarMokepon()
 */
 
-    mokeponEnemigo.pintarMokepon()
+    //mokeponEnemigo.pintarMokepon()
 
     if(mascotaJugadorObjeto.velocidadX != 0 || mascotaJugadorObjeto.velocidadY != 0){
         revisarColision(hipodogeEnemigo)
@@ -489,7 +495,7 @@ function enviarPosicion(x,y){
             res.json()
                 .then(function ({enemigos}){
                 console.log(enemigos)
-                enemigos.forEach(function(enemigo){
+                mokeponesEnemigos = enemigos.map(function(enemigo){
                     let mokeponEnemigo = null
                     const mokeponNombre = enemigo.mokepon.nombre || ""
                     if (mokeponNombre === "Hipodoge"){
@@ -505,7 +511,9 @@ function enviarPosicion(x,y){
                     mokeponEnemigo.x = enemigo.x
                     mokeponEnemigo.y = enemigo.y
                     
-                    mokeponEnemigo.pintarMokepon()
+                    //mokeponEnemigo.pintarMokepon()
+
+                    return mokeponEnemigo
                 })
             })
         }
